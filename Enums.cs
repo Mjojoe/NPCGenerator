209c931fae79efda3757d0c738b9d14c7717608e
Moritz Fenzl <if20b081@technikum-wien.at>
2024-06-22 19:58:11 +0200

    Add project files.

---
+++ Enums.cs
@@ -0,0 +1,42 @@
+﻿namespace DoorGenerator
+{
+   enum Oppenness
+    {
+        OPEN,
+        CLOSE,
+        LEANED
+    }
+    enum Material
+    {
+        WOOD,
+        STONE,
+        METAL,
+        GLASS,
+        FABRIC
+    };
+
+    enum Construction
+    {
+        SOLID,
+        DECREPID,
+        BARS,
+        WINDOWED
+    };
+
+    enum Locked
+    {
+        LOCKED,
+        MULTIPLE,
+        UNLOCKED,
+        BARRED,
+    };
+
+    enum Locktype
+    {
+        INTEGRATED,
+        PADLOCK,
+        BAR,
+        CHAIN,
+        CLUTTER
+    };
+}
